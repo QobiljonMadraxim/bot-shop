77ff0000956563cb32901b4ee4a94ba178c13905
QobiljonMadraxim <116558613+QobiljonMadraxim@users.noreply.github.com>
2022-10-24 16:43:39 +0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -24,20 +24,23 @@ class OpenShopParser:
         data = []
         soup = self.get_soup()
         box = soup.find('div', class_='product-wrapper row cols-lg-4 cols-md-3 cols-sm-2 cols-2')
-        products = box.find_all('div', class_='product-wrap')
-        for product in products:
-            title = product.find('h3').get_text(strip=True)
-            link = product.find('a')['href']
-            price = int(product.find('ins', class_='new-price').get_text(strip=True).split('UZS')[0].replace(' ', ''))
-            image = product.find('img')['src']
-
-            cat_id = db.select_category_by_cat_name(self.category)[0]
-
-            data.append({
-                'title': title,
-                'link': link,
-                'price': price,
-                'image': image,
-                'category_id': cat_id
-            })
+        try:
+            products = box.find_all('div', class_='product-wrap')
+            for product in products:
+                title = product.find('h3').get_text(strip=True)
+                link = product.find('a')['href']
+                price = int(product.find('ins', class_='new-price').get_text(strip=True).split('UZS')[0].replace(' ', ''))
+                image = product.find('img')['src']
+    
+                cat_id = db.select_category_by_cat_name(self.category)[0]
+    
+                data.append({
+                    'title': title,
+                    'link': link,
+                    'price': price,
+                    'image': image,
+                    'category_id': cat_id
+                })
+        except:
+            pass
         return data
